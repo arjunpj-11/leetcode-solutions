fbfadfe046250188785488adb4ee5e7c0cdcdd9a
Arjun PJ <arjunpj11@gmail.com>
2026-06-08 15:36:07 +0530

    Time: 80 ms (6.26%), Space: 65.2 MB (5.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,11 +1,13 @@
 var twoSum = function(nums, target) {
-    let a = new Map()
-    for(i=0;i<nums.length;i++){
-        let c = target- nums[i]
-        if(a.has(c)){
-            return [a.get(c),i]
-        }
-        a.set(nums[i],i)
-    }
-    return [];
+   let a = new Map()
+   for(let i=0; i<nums.length; i++){
+     let sum = target - nums[i]
+
+     console.log(a.get(2))
+     if(a.has(sum)){
+        return [i, a.get(sum)]
+     }
+     a.set(nums[i], i)
+   }
+   return []
 };
